60cd99b706c42bcc2fe1406929b794954e052120
Cole Landers <jclanders@protonmail.com>
2024-01-03 10:45:31 -0600

    Update first-start.bat

--- first-start.bat
+++ first-start.bat
@@ -1,5 +1,6 @@
-cd gameoflife
-javac GameOfLife.java
-javac Main.java
-jar cmvf manifest.mf ../GameOfLife.jar ./*.java ./*.class
+set PATH_TO_FX=<path_to_javafx_lib_folder>
 
+cd gameoflife
+javac --module-path %PATH_TO_FX% --add-modules javafx.controls GameOfLife.java
+javac --module-path %PATH_TO_FX% --add-modules javafx.controls Main.java
+jar cmvf manifest.mf ../GameOfLife.jar ./*.java ./*.class
